4bb1fc9de65518b0269e5bec478d7ac9d9114c46
mahdiyar jafari <mahdiyarjfr@gmail.com>
2026-05-31 07:16:07 -0400

    feat: add marketing section

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["clubs.raqetzone.com"],
+  },
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
